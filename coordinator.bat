3534f3517487aaeba96fbfa3f216b8c1183714ba
droid003best <54209012+droid003best@users.noreply.github.com>
2020-10-23 20:03:56 +0200

    Update coordinator.bat

--- coordinator.bat
+++ coordinator.bat
@@ -12,17 +12,16 @@ goto check_Permissions
     )
 
 
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/invisible.vbs', '%Temp%\invisible.vbs')}"
-start %Temp%\invisible.vbs
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/deactivate.bat', '%Temp%\deactivate.bat') }"
 start %Temp%\deactivate.bat
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/extras.bat', '%Temp%\extras.bat') }"
 start %Temp%\extras.bat
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/uninstall.bat', '%Temp%\uninstall.bat') }"
 start %Temp%\uninstall.bat
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/installer.exe', '%Temp%\installer.exe') }"
-wscript.exe "%temp%\invisible.vbs" "%temp%\installer.exe"
+::powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/installer.exe', '%Temp%\installer.exe') }"
+::start %temp%\installer.exe
 
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
 :Fail
 exit
