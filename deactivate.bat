334ebea785458280c2189b4f9c9cb5ab684a7b99
droid003best <54209012+droid003best@users.noreply.github.com>
2020-11-22 00:25:57 +0100

    Update deactivate.bat

--- deactivate.bat
+++ deactivate.bat
@@ -112,8 +112,8 @@ IF EXIST "choco.exe" (
 
 :2
 choco install googlechrome -y
-start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install winrar -y"
-start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install filezilla -y"
-start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install vlc -y"
-pause
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install winrar -y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install filezilla -y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install vlc -y & exit"
+
 exit
