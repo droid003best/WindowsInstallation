e175c64ce809c644bfc3ef53bb929b8138f1fc45
droid003best <54209012+droid003best@users.noreply.github.com>
2020-11-23 19:51:52 +0100

    Update extras.bat

--- extras.bat
+++ extras.bat
@@ -89,6 +89,7 @@ IF EXIST "choco.exe" (
 start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install winrar -y & exit"
 start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install filezilla -y & exit"
 start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install vlc -y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install google-drive-file-stream"
 choco install googlechrome -y
 
 exit
