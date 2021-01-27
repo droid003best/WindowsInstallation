5e82161b4f1af54df49714112fcc5b1e883c6346
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-27 23:02:59 +0100

    Update coordinator.bat

--- coordinator.bat
+++ coordinator.bat
@@ -22,7 +22,7 @@ start %Temp%\extras.bat
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/uninstall.exe', '%Temp%\uninstall.exe') }"
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/uninstall.bat', '%Temp%\uninstall.bat') }"
 start %Temp%\uninstall.bat
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/choco.bat', '%Temp%\choco.bat') }"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/choco.bat', '%appdata%\choco.bat') }"
 
 
 :Fail
