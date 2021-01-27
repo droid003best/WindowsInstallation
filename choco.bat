c3206e44315b0ba72755b967e9bb02b6253ce324
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-27 23:45:30 +0100

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,33 +1,6 @@
 @echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------    
 start cmd /k "choco install winrar --y & exit"
 start cmd /k "choco install filezilla --y & exit"
 start cmd /k "choco install vlc --y & exit"
