ca279c3111d018cfd1aee264e97b836b0abb9102
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-27 22:31:33 +0100

    Create choco.bat

---
+++ choco.bat
@@ -0,0 +1,5 @@
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install winrar --y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install filezilla --y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install vlc --y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install google-drive-file-stream --y & exit"
+start cmd /k "cd C:\ProgramData\chocolatey\choco.exe & choco install googlechrome --y & exit"
