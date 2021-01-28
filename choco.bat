6e16c9fbc97cfbc162b10fbba23515bbde8e620d
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-28 10:20:06 +0100

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,8 +1,8 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-start cmd /k "choco install winrar --y & exit"
-start cmd /k "choco install filezilla --y & exit"
-start cmd /k "choco install vlc --y & exit"
-start cmd /k "choco install google-drive-file-stream --y & exit"
-start cmd /k "choco install googlechrome --y & exit & del %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\choco.bat"
+cmd /k "choco install winrar --y & exit" > %temp%\wSetup\winrar.txt
+cmd /k "choco install filezilla --y & exit" > %temp%\wSetup\filezilla.txt
+cmd /k "choco install vlc --y & exit" > %temp%\wSetup\vlc.txt
+cmd /k "choco install google-drive-file-stream --y & exit" > %temp%\wSetup\drive.txt
+cmd /k "choco install googlechrome --y & exit & del %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\choco.bat" > %temp%\wSetup\chrome.txt
