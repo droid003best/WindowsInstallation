5f97b77dd70562ea958d168f6bf2774f0203c264
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-28 11:13:31 +0100

    Update coordinator.bat

--- coordinator.bat
+++ coordinator.bat
@@ -13,7 +13,7 @@ powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.gi
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/uninstall.bat', '%Temp%\uninstall.bat') }"
 start %Temp%\uninstall.bat
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/WindowsInstallation/main/choco.bat', '%temp%\choco.bat') }"
-start powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" > %temp%\wSetup\Chocolatey-install
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" > %temp%\wSetup\Chocolatey-install
 SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\choco"
 SCHTASKS /RUN /TN "choco"
 SCHTASKS /DELETE /TN "choco" /f
